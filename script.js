e4d3c8163b0b95ba231105f224de59a9ef8c6a33
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2024-03-08 15:58:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Your code here.
+let draggedItem = null;
+
+    document.addEventListener('dragstart', function (event) {
+      draggedItem = event.target;
+      event.dataTransfer.setData('text/plain', ''); // required for Firefox
+    });
+
+    document.addEventListener('dragover', function (event) {
+      event.preventDefault();
+    });
+
+    document.addEventListener('drop', function (event) {
+      event.preventDefault();
+      if (event.target.classList.contains('item')) {
+        const targetItem = event.target;
+        swapItems(draggedItem, targetItem);
+      }
+    });
+
+    function swapItems(item1, item2) {
+      const temp = document.createElement('div');
+      item1.parentNode.insertBefore(temp, item1);
+      item2.parentNode.insertBefore(item1, item2);
+      temp.parentNode.insertBefore(item2, temp);
+      temp.parentNode.removeChild(temp);
+    }
